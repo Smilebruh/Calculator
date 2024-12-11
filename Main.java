9a6d01b4fb16410443cfa5b9b01b3fbfd0875e85
smilehacking27@gmail.com <dimasalexander27@gmail.com>
2024-12-11 22:43:14 +0700

    adding number button

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
+import java.awt.event.*;
 
 class Main{
-    private void buttonhandler(){
-        //
+    private static void buttonhandler(String text,JLabel lab){
+        System.out.println("the button has pressed"+text);
+
+        lab.setText(lab.getText() + text);
     }
 
     public static void main(String[] args){
@@ -27,6 +30,10 @@ class Main{
         ui.setPreferredSize(new Dimension(280,100));
         ui.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
 
+        JLabel label = new JLabel(" ");
+        label.setSize(280,100);
+        ui.add(label);
+
         JPanel component = new JPanel();
         component.setMaximumSize(new Dimension(280,350));
         component.setMinimumSize(new Dimension(280,350));
@@ -35,10 +42,13 @@ class Main{
         component.setLayout(new BoxLayout(component,BoxLayout.X_AXIS));
 
         JPanel div1 = new JPanel();
-        div1.setBackground(Color.BLACK);
+        div1.setLayout(new GridBagLayout());
+
 
         JPanel div2 = new JPanel();
         div2.setBackground(Color.WHITE);
+        div2.setLayout(new GridBagLayout());
+
         component.add(div1);
         component.add(div2);
 
@@ -47,27 +57,35 @@ class Main{
 
         //make the component
 
-//        JButton[] button = new JButton[10];
-//        {
-//            byte index = 0;
-//            for (byte i=0;i<3;i++){
-//                for (byte j = 0;j<3;j++) {
-//                    button[index] = new JButton(Integer.toString(index + 1));
-//                    gbc.gridx = j;
-//                    gbc.gridy = i;
-//                    gbc.anchor = GridBagConstraints.NORTHWEST;
-//                    gbc.weightx = 0;
-//                    gbc.weighty = 0;
-//
-//                    component.add(button[index], gbc);
-//                    index++;
-//                }
-//            }
-//            button[9] = new JButton("0");
-//            gbc.gridx = 1;
-//            gbc.gridy = 3;
-//            component.add(button[9],gbc);
-//        }
+        JButton[] button = new JButton[10];
+        {
+            byte index = 0;
+            for (byte i=0;i<3;i++){
+                for (byte j = 0;j<3;j++) {
+                    final byte constant = (byte)(index+1);
+                    button[index] = new JButton(Integer.toString(index + 1));
+                    button[index].setPreferredSize(new Dimension(40,40));
+                    button[index].addActionListener(event -> buttonhandler(Byte.toString(constant),label));
+                    gbc.gridx = j;
+                    gbc.gridy = i;
+                    gbc.anchor = GridBagConstraints.NORTHWEST;
+                    gbc.weightx = 0;
+                    gbc.weighty = 0;
+
+                    div1.add(button[index], gbc);
+                    index++;
+                }
+            }
+            button[9] = new JButton("0");
+            button[9].setPreferredSize(new Dimension(40,40));
+            button[9].addActionListener(event -> buttonhandler("0",label));
+            gbc.gridx = 1;
+            gbc.gridy = 3;
+            gbc.anchor =  GridBagConstraints.NORTHWEST;
+            gbc.weighty = 0;
+            gbc.weightx = 0;
+            div1.add(button[9],gbc);
+        }
 
         window.add(Box.createVerticalStrut(5));
         window.add(ui);
